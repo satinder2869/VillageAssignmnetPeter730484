d566d0445a1581e61b2106f3f63022dfc15ba6a5
visualstudio <visualstudio@230-PC21>
2019-01-31 15:28:17 -0500

    commit

--- VillageAssignmnetPeter730484/Program.cs
+++ VillageAssignmnetPeter730484/Program.cs
@@ -6,7 +6,63 @@ namespace VillageAssignmnetPeter730484
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Countryside Ontario = new Countryside();
+            Ontario.run();
         }
     }
+    class Village
+    {
+        public static int numberofVillages = 0;
+        public Village nextVillage;
+        public Village previousVillage;
+        public string VillageName;
+        public bool isAstrildeHere = false;
+
+        public Village() { Village.numberofVillages++; }
+    }
+    class Countryside
+    {
+        public Village Maple;
+
+        public Village Toronto;
+        public Village Ajax;
+        public Village Head;
+        public Village Tail;
+        public Village Temp;
+        public void run()
+        {
+            this.MapInitializer();
+            this.LookForAstrilde();
+        }
+
+        public void MapInitializer()
+        {
+            Maple = new Village();
+            Maple.VillageName = "Maple";
+            Maple.previousVillage = null;
+            Maple.nextVillage = Toronto;
+            Toronto = new Village();
+            Toronto.previousVillage = Maple;
+            Toronto.VillageName = "Toronto";
+            Toronto.nextVillage = Ajax;
+            Ajax = new Village();
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.previousVillage = Toronto;
+            //Ajax.isAstrildeHere= true;
+
+        }
+        public void LookForAstrilde()
+        {
+            Head = Maple;
+            if (Head.isAstrildeHere)
+            {
+                Console.WriteLine("Yeah ! Astrilde is in");
+            }
+            //while(true){
+            // }
+
+        }
+    }
+
 }
