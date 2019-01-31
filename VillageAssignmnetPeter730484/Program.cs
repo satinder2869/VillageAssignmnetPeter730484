b7bafda23a88ac84b5cdeef17bcaadc1bdde62b2
visualstudio <visualstudio@230-PC21>
2019-01-31 15:27:48 -0500

    730484

---
+++ VillageAssignmnetPeter730484/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VillageAssignmnetPeter730484
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
